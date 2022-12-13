02c9077afd22a10c647f9b270b9b9b00e73c3f76
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-12-13 10:45:47 -0300

    Aula 17 - Router parte 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link :to="rotaDinamica">Serviços</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      rotaDinamica: { name: "servicos" },
+    };
+  },
+};
+</script>
 
 <style>
 #app {
